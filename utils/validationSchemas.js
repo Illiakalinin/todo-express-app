b79ca8aee99ccea7602ce3d45639acc220b47932
illiakalinin <illinoiz4305@gmail.com>
2023-03-30 20:37:54 +0300

    Add validation

---
+++ utils/validationSchemas.js
@@ -0,0 +1,15 @@
+const yup = require('yup');
+
+const NAME_VALIDATION_SCHEMA = yup.string().trim().min(2).max(200);
+
+module.exports.CREATE_TASK_VALIDATION_SCHEMA = yup.object({
+  name: NAME_VALIDATION_SCHEMA,
+  createdAt: yup.date().max(new Date()),
+  isDone: yup.boolean(),
+});
+
+module.exports.UPDATE_TASK_VALIDATION_SCHEMA = yup.object({
+  name: NAME_VALIDATION_SCHEMA,
+  createdAt: yup.date().max(new Date()),
+  isDone: yup.boolean(),
+});
