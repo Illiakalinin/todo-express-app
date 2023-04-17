27d1f8488a9ecad9dd10523b0656ed52548669e4
illiakalinin <illinoiz4305@gmail.com>
2023-04-17 17:28:09 +0300

    Add required

--- utils/validationSchemas.js
+++ utils/validationSchemas.js
@@ -3,7 +3,7 @@ const yup = require('yup');
 const NAME_VALIDATION_SCHEMA = yup.string().trim().min(2).max(200);
 
 module.exports.CREATE_TASK_VALIDATION_SCHEMA = yup.object({
-  name: NAME_VALIDATION_SCHEMA,
+  name: NAME_VALIDATION_SCHEMA.required(),
   createdAt: yup.date().max(new Date()),
   isDone: yup.boolean(),
 });
